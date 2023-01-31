de5471caf27d8cff0b8d742a1cb825375abe9de2
Kevin Quillen <15399-gh0st25@users.noreply.drupalcode.org>
2023-01-31 16:08:35 -0500

    Issue #3337947 by kevinquillen: Make log level options configurable

--- modules/openai_dblog/src/Controller/OpenAIDbLogController.php
+++ modules/openai_dblog/src/Controller/OpenAIDbLogController.php
@@ -6,7 +6,7 @@ use Drupal\dblog\Controller\DbLogController;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Overrides the dblog detail page to provide OpenAI powered answers.
+ * Overrides the log details page to provide OpenAI powered explanations.
  */
 class OpenAIDbLogController extends DbLogController {
 
@@ -32,15 +32,23 @@ class OpenAIDbLogController extends DbLogController {
   public function eventDetails($event_id) {
     $build = parent::eventDetails($event_id);
     $rows = $build['dblog_table']['#rows'];
+    $severity = trim(strip_tags($rows[6][1]->render()));
+    $config = $this->config('openai_dblog.settings');
+    $levels = $config->get('levels');
 
-    // @todo Only fire this for warnings to critical level messages.
+    if (!array_key_exists($severity, $levels)) {
+      return $build;
+    }
+
+    $result = [];
+
     try {
       $message = trim(strip_tags($rows[5][1]->render()));
 
       $response = $this->client->completions()->create(
         [
           'model' => 'text-davinci-003',
-          'prompt' => 'What does this error mean? The error is: ' . $message,
+          'prompt' => 'What does this error mean? The error is: ' . $message . ' How can I fix this? Explain like I am 5.',
           'temperature' => 0.4,
           'max_tokens' => 256,
         ],
@@ -52,17 +60,19 @@ class OpenAIDbLogController extends DbLogController {
       // @todo Catch and log error
     }
 
-    $rows[] = [
-      [
-        'data' => $this->t('Possible solution'),
-        'header' => TRUE,
-      ],
-      [
-        'data' => [
-          '#markup' => $result["choices"][0]["text"] ?? 'No solutions were found.',
+    if (!empty($result)) {
+      $rows[] = [
+        [
+          'data' => $this->t('Explanation (powered by <a href="@link">OpenAI</a>)', ['@link' => 'https://openai.com']),
+          'header' => TRUE,
         ],
-      ],
-    ];
+        [
+          'data' => [
+            '#markup' => $result["choices"][0]["text"] ?? 'No possible explanations were found, or the API service is not responding.',
+          ],
+        ],
+      ];
+    }
 
     $build['dblog_table']['#rows'] = $rows;
     return $build;
